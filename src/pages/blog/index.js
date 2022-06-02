a5bb822b4000db227906a8c4e148a5554a982b0e
chuchu02 <rlaxogns73@gmail.com>
2022-06-02 09:55:35 +0900

    블로그를 통해서 포스트페이지 가기

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -12,12 +12,12 @@ const BlogPage = ({ data }) => {
             <Link to={`/blog/${node.slug}`}>{node.frontmatter.title}</Link>
           </h2>
           <p>Posted: {node.frontmatter.date}</p>
-          <StaticImage
-            alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
-            src="https://cdn.imweb.me/upload/S201712205a3a0910b89f5/d95b7faed6d64.jpg"
-          />
         </article>
       ))}
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="https://cdn.imweb.me/upload/S201712205a3a0910b89f5/d95b7faed6d64.jpg"
+      />
     </Layout>
   );
 };
