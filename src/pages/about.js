4e0e6f3ed0baf9c11e72b3e95f8ae66c11bee237
chuchu02 <rlaxogns73@gmail.com>
2022-06-02 09:10:43 +0900

    블로그 글 통해서 포스트페이지로 move

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,17 +1,18 @@
+import { StaticImage } from "gatsby-plugin-image";
 import React from "react";
 import Layout from "../components/layout";
 
 function AboutPage() {
   return (
-    
     <Layout pageTitle="About me">
       <p>
-      Hi there! I'm the proud creator of this site, which I built with
-          Gatsby.
+        Hi there! I'm the proud creator of this site, which I built with Gatsby.
       </p>
-      </Layout>
-    
-    
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/202001/01/f86957cb-ee94-4611-bc81-a5478ca91f92.jpg"
+      />
+    </Layout>
   );
 }
 
