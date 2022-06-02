25acea2a512bb045c15d2faf454c2b5f623671d7
chuchu02 <rlaxogns73@gmail.com>
2022-06-02 09:52:44 +0900

    블로그 통해서 포스트 받아오기

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,4 +1,5 @@
 import { graphql, Link } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
 import React from "react";
 import Layout from "../../components/layout";
 
@@ -11,6 +12,10 @@ const BlogPage = ({ data }) => {
             <Link to={`/blog/${node.slug}`}>{node.frontmatter.title}</Link>
           </h2>
           <p>Posted: {node.frontmatter.date}</p>
+          <StaticImage
+            alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+            src="https://cdn.imweb.me/upload/S201712205a3a0910b89f5/d95b7faed6d64.jpg"
+          />
         </article>
       ))}
     </Layout>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,17 @@
-import * as React from "react"
-import Layout from "../components/layout"
-import { StaticImage } from 'gatsby-plugin-image';
+import * as React from "react";
+import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 // markup
 const IndexPage = () => {
   return (
-    
-      <Layout pageTitle="Home Page">
+    <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
       <StaticImage
         alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
-        src="../images/dog.jpg"
+        src="https://dimg.donga.com/wps/NEWS/IMAGE/2022/01/28/111500268.2.jpg"
       />
-      </Layout>
-    
-  )
-}
+    </Layout>
+  );
+};
 
-export default IndexPage
+export default IndexPage;
